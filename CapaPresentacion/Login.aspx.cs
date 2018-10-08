63f50c2fe51f7d175c9cc25bc060e0c6d887cb13
Antonio Delgado <adelgado.mx@hotmail.com>
2018-10-08 02:27:02 -0500

    Agregar CapaPresentacion y formulario de login estatico, incluye directorios css, js, fonts e img

---
+++ CapaPresentacion/Login.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnIngresar_Click(object sender, EventArgs e)
+    {
+        string usuario = txtUsuario.Text;
+        string password = txtPassword.Text;
+
+        if ("adelgado".Equals(usuario) && "4ug02000".Equals(password))
+            Response.Write("<script>alert('Usuario correcto!')</script>");
+        else
+            Response.Write("<script>alert('Usuario incorrecto!')</script>");
+    }
+}
